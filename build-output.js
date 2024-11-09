de0e63789bd7b07585de824da198247ea56d7861
mark <markhuang1993@gmail.com>
2024-11-09 22:09:33 +0800

    add build scripts and github action

---
+++ build-output.js
@@ -0,0 +1,31 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+
+
+register(StyleDictionary, {
+  excludeParentKeys: true,
+});
+
+const sd = new StyleDictionary({
+  // make sure to have source match your token files!
+  // be careful about accidentally matching your package.json or similar files that are not tokens
+  source: ['tokens/**/*.json'],
+  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
+      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
